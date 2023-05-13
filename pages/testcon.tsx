d61d04fc827248366ae3e17036f283f7c1f3fc1c
shawhu <shawhu@gmail.com>
2023-05-13 12:48:49 +0800

    testing the chat layout in testcon page

--- pages/testcon.tsx
+++ pages/testcon.tsx
@@ -1,46 +1,61 @@
 import React from "react";
-import { Grid, Paper, Typography } from "@mui/material";
+import {
+  List,
+  ListItem,
+  ListItemText,
+  Typography,
+  ListSubheader,
+} from "@mui/material";
 
 function ConversationPage() {
   const messages = [
     { sender: "John", text: "Hello Mary, how are you?" },
     { sender: "Mary", text: "Hey John, I'm doing well. How about you?" },
-    { sender: "John", text: "I'm good, thanks for asking" },
+    { sender: "John", text: "I'm good, thanks for asking." },
     { sender: "Mary", text: "What have you been up to lately?" },
     {
       sender: "John",
-      text: "Not much, just working and hanging out with friends. How about you?",
+      text: "Not much, just hanging out with friends. How about you?",
     },
     {
       sender: "Mary",
-      text: "I had a busy week with work, but looking forward to the weekend",
+      text: "I had a busy week with work, but looking forward to the weekend.",
+    },
+    {
+      sender: "Mary",
+      text: "Me too! Let's catch up then.",
     },
     // Add more message objects here as needed
   ];
 
   return (
-    <Grid container spacing={2}>
-      <Grid item xs={12}>
-        <Typography variant="h4">Conversations with Mary</Typography>
-      </Grid>
-      <Grid item xs={12}>
-        {messages.map((message, index) => (
-          <Paper
-            key={index}
-            elevation={3}
-            className={`p-4 rounded-lg ${
-              message.sender === "John"
-                ? "float-left bg-white"
-                : "float-right bg-pink-100"
+    <List>
+      <ListSubheader className="font-bold text-2xl ">
+        Conversations between John and Mary
+      </ListSubheader>
+      {messages.map((message, index) => (
+        <ListItem
+          key={index}
+          className={`flex ${
+            message.sender === "John"
+              ? "flex-row justify-start"
+              : "flex-row-reverse"
+          }`}
+        >
+          <ListItemText
+            primary={message.text}
+            secondary={message.sender}
+            className={`rounded-t-xl p-4 max-w-[80%] ${
+              message.sender === "John" ? "rounded-br-xl" : "rounded-bl-xl"
+            } ${message.sender === "John" ? "bg-blue-500" : "bg-green-500"}
+            ${message.sender === "John" ? "text-white" : "text-white"} ${
+              message.sender === "John" ? "text-left" : "text-right"
             }`}
-          >
-            <Typography variant="body1">
-              {message.sender}: {message.text}
-            </Typography>
-          </Paper>
-        ))}
-      </Grid>
-    </Grid>
+          />
+          <div>adsf</div>
+        </ListItem>
+      ))}
+    </List>
   );
 }
 
