0d8b2380b8b21a3c6d1a70d07d9eb0786281a051
shawhu <shawhu@gmail.com>
2023-05-13 14:40:56 +0800

    fix some issues

--- pages/testcon.tsx
+++ pages/testcon.tsx
@@ -7,14 +7,14 @@ import {
   ListSubheader,
   ListItemAvatar,
   Avatar,
-  FolderIcon,
 } from "@mui/material";
+import FolderIcon from "@mui/icons-material/Folder";
 
 function ConversationPage() {
   const messages = [
     { sender: "John", text: "Hello Mary, how are you?" },
     { sender: "Mary", text: "Hey John, I'm doing well. How about you?" },
-    { sender: "John", text: "I'm good, thanks for asking." },
+    { sender: "John", text: "I'm good, <strong>thanks</strong> for asking." },
     { sender: "Mary", text: "What have you been up to lately?" },
     {
       sender: "John",
@@ -22,7 +22,7 @@ function ConversationPage() {
     },
     {
       sender: "Mary",
-      text: "I had a busy week with work, but looking forward to the weekend.",
+      text: "I had a busy week with work, but looking forward to the weekend. I had a busy week with work, but looking forward to the weekend. I had a busy week with work, but looking forward to the weekend.",
     },
     {
       sender: "Mary",
@@ -39,22 +39,24 @@ function ConversationPage() {
       {messages.map((message, index) => (
         <ListItem
           key={index}
-          className={`flex ${
-            message.sender === "John"
-              ? "flex-row justify-start"
-              : "flex-row-reverse"
+          alignItems="flex-start"
+          className={`flex justify-start ${
+            message.sender === "John" ? "flex-row " : "flex-row-reverse"
           }`}
         >
           <ListItemText
-            primary={message.text}
-            secondary={message.sender}
+            primary={<div dangerouslySetInnerHTML={{ __html: message.text }} />}
+            secondary={message.sender + " 8:13 AM"}
             className={`rounded-t-xl p-4 max-w-[80%] ${
               message.sender === "John" ? "rounded-br-xl" : "rounded-bl-xl"
-            } ${message.sender === "John" ? "bg-blue-500" : "bg-green-500"}
-            ${message.sender === "John" ? "text-white" : "text-white"} ${
+            } ${message.sender === "John" ? "bg-blue-50" : "bg-green-50"}
+            ${message.sender === "John" ? "text-black" : "text-black"} ${
               message.sender === "John" ? "text-left" : "text-right"
             }`}
           />
+          <ListItemAvatar className="flex justify-center">
+            <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+          </ListItemAvatar>
         </ListItem>
       ))}
     </List>
---
+++ pages/testlist.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import Divider from "@mui/material/Divider";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
+
+export default function AlignItemsList() {
+  return (
+    <List sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+      <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
+        </ListItemAvatar>
+        <ListItemText
+          primary="Brunch this weekend?"
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: "inline" }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+                Ali Connors
+              </Typography>
+              {
+                " — I'll be in your neighborhood doing errands this… I'll be in your neighborhood doing errands this… I'll be in your neighborhood doing errands this…"
+              }
+            </React.Fragment>
+          }
+        />
+      </ListItem>
+    </List>
+  );
+}
