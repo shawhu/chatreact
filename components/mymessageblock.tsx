7b76d87eabf3b1d621bab57efdf5d04c6cfa13fd
shawhu <shawhu@gmail.com>
2023-05-26 11:12:24 +0800

    fix the regex to extract code blocks it will match code with no ending ``` as well now.

--- components/mymessageblock.tsx
+++ components/mymessageblock.tsx
@@ -9,18 +9,18 @@ import {
 } from "react-syntax-highlighter/dist/cjs/styles/hljs";
 //docs reference
 //https://github.com/react-syntax-highlighter/react-syntax-highlighter/blob/master/AVAILABLE_STYLES_HLJS.MD
-
+//this is the actual block that display the content returned by AI LLM
 export default function MyMessageBlock({ rawtext }) {
   const [displaytexts, setDisplaytexts] = useState([]);
   useEffect(() => {
     //using regex to replace ```
-    const regex = /```\s?([\s\S]*?)\s?```/g;
-    const matches = Array.from(rawtext.matchAll(regex));
-    const codeblocks = matches.map((match) => match[1]);
-    const processed = rawtext.replace(
-      regex, //\s is whitespace including \n, \S is character
-      `codeblock`
+    const regex = /```([^`]+?)```|```([^`]+?)$/g;
+    //             ^match[1]       ^match[2]
+    const matchesArray = Array.from(rawtext.matchAll(regex));
+    const codeblocks = matchesArray.map((match) =>
+      match[1] ? match[1] : match[2]
     );
+    const processed = rawtext.replace(regex, `codeblock`);
     //here we add first block of text. if no code found, first block
     // is the one and only block.
     const textblocks = processed.split("codeblock");
@@ -30,10 +30,15 @@ export default function MyMessageBlock({ rawtext }) {
     //if there's code block, we then add <code><text>,<code><text>, until the end
     if (textblocks.length > 1) {
       for (let i = 1; i < textblocks.length; i++) {
+        //check to see if codeblock is empty
+        if (!codeblocks[i - 1]) {
+          break;
+        }
         //processing codeblock to get first line and the rest
         const codelines = codeblocks[i - 1].split("\n");
         const language = codelines[0];
-        const actualcode = codelines.slice(1).join("\n");
+        //const actualcode = codelines.slice(1).join("\n"); //this removes the 1st line
+        const actualcode = codelines.join("\n"); //this wont
         tempdisplaytexts.push({
           type: "code",
           content: actualcode,
