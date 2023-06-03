9e55fa66c3aa34b886065e17dd461450b74db45e
shawhu <shawhu@gmail.com>
2023-06-03 21:23:26 +0800

    added some voice over api testing

--- components/conversation.tsx
+++ components/conversation.tsx
@@ -191,9 +191,16 @@ function Conversation({
           {audioText != "" && voiceover ? (
             <audio
               ref={audioRef}
-              src={`http://localhost:5002/api/tts?text=${encodeURIComponent(
-                audioText
-              )}&speaker_id=p248&style_wav=&language_id=`}
+              src={
+                `http://localhost:5002/api/tts?text=${encodeURIComponent(
+                  audioText
+                )}&speaker_id=p248&style_wav=&language_id=`
+
+                //'female-en-5': 0, 'female-en-5\n': 1, 'female-pt-4\n': 2, 'male-en-2': 3, 'male-en-2\n': 4, 'male-pt-3
+                // `http://localhost:5002/api/tts?text=${encodeURIComponent(
+                //   audioText
+                // )}&speaker_id=female-en-5&style_wav=&language_id=en`
+              }
               controls
               autoPlay
             />
