a2eca87034f6d12c443cc5f6e3cc775cb4f941c9
shawhu <shawhu@gmail.com>
2023-06-14 10:53:37 +0800

    fix a bug that causes by missing sessions dir

--- pages/api/sessionsload.ts
+++ pages/api/sessionsload.ts
@@ -2,10 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import path from "path";
 import { promises as fs } from "fs";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "GET") {
     // sessionid is undefined, we shall return all sessions
     const sessions = await LoadAllSessions();
@@ -22,28 +19,18 @@ async function LoadAllSessions() {
       await fs.access(jsonDirectory);
     } catch (error) {
       console.log(`Directory ${jsonDirectory} doesn't exist, create a new one`);
-      fs.mkdir(jsonDirectory);
+      await fs.mkdir(jsonDirectory);
     }
 
     const files = await fs.readdir(jsonDirectory);
     if (files.length == 0) {
       //sessions folder is empty, we should create them from sessions_template.json
-      const template_file = path.join(
-        process.cwd(),
-        "json/sessions_template.json"
-      );
+      const template_file = path.join(process.cwd(), "json/sessions_template.json");
       const template = await fs.readFile(template_file, "utf-8");
       const template_sessions = JSON.parse(template);
       template_sessions.forEach(async (session: any) => {
-        const sessionfile = path.join(
-          process.cwd(),
-          `json/sessions/${session.sessionId}.json`
-        );
-        const outcome = await fs.writeFile(
-          sessionfile,
-          JSON.stringify(session, null, 2),
-          "utf-8"
-        );
+        const sessionfile = path.join(process.cwd(), `json/sessions/${session.sessionId}.json`);
+        const outcome = await fs.writeFile(sessionfile, JSON.stringify(session, null, 2), "utf-8");
       });
       return template_sessions;
     }
